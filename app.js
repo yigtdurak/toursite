faa3b3197df873c737b6b7bfa8dd467fbfcb9139
Yigit Durak <yigtdurak@gmail.com>
2026-03-18 18:45:59 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function on_mouse(id) {
+    let select = document.getElementById(id);
+    select.style.width = "100%";
+}
+function out_mouse(id){
+    let select = document.getElementById(id);
+    select.style.width = "0";
+}
